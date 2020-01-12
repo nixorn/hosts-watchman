e16e1a5d17b4e7bb77b4eadcce7c149ce9808d5d
Artem Kanev <a-kanev@yandex.ru>
2020-01-12 19:26:18 +0300

    First version. Implemented reading tshark logs from journalctl and
    writing hosts to 3 files: untrusted, trusted and uncheked.

--- app/hosts-watchman/Main.hs
+++ app/hosts-watchman/Main.hs
@@ -1,13 +1,39 @@
 module Main where
 
+import           Data.Set                        (Set, (\\))
 import qualified Data.Set                        as S
+import qualified Data.Text                       as T
 import qualified Data.Text.IO                    as T
+import           System.Process                  (readProcess)
 import           Text.Megaparsec                 (parse)
 
 import           HostsWatchman.Parsers.Wireshark
 
+tsharkServiceName :: String
+tsharkServiceName = "tshark"
+
+trustedHostsPath :: String
+trustedHostsPath = "./trusted_hosts"
+
+untrustedHostsPath :: String
+untrustedHostsPath = "./untrusted_hosts"
+
+uncheckedHostsPath :: String
+uncheckedHostsPath = "./unchecked_hosts"
+
+readDomains :: FilePath -> IO (Set Domain)
+readDomains path = S.fromList . map Domain . T.lines <$> T.readFile path
+
 main :: IO ()
 main = do
-  forParse <- T.readFile "/var/log/tshark.log"
-  let Right result = S.fromList <$> parse hostMapsParser "/var/log/tshark.log" forParse
-  print result
+  trusted   <- readDomains trustedHostsPath
+  untrusted <- readDomains untrustedHostsPath
+  unchecked <- readDomains uncheckedHostsPath
+  forParse  <- T.pack <$> readProcess "journalctl" ["-t", tsharkServiceName] ""
+  let Right parsed  = S.fromList <$> parse domainsParser "tshark.log" forParse
+  T.writeFile uncheckedHostsPath
+    $ T.unlines
+    $ map unDomain
+    $ S.toList
+    $ S.union unchecked
+    $ (parsed \\ trusted) \\ untrusted
--- src/HostsWatchman/Parsers/Wireshark.hs
+++ src/HostsWatchman/Parsers/Wireshark.hs
@@ -1,47 +1,46 @@
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
 module HostsWatchman.Parsers.Wireshark
-    ( hostMapsParser
+    ( Domain (..)
+    , domainsParser
     ) where
 
 import           Control.Applicative       ((<|>))
 import           Control.Monad.Combinators (many)
-import           Data.Text                 (Text, pack)
+import           Data.Maybe                (catMaybes)
+import           Data.Text                 (Text)
+import qualified Data.Text                 as T
 import           Data.Void                 (Void)
 import           Text.Megaparsec           (Parsec, try)
 import           Text.Megaparsec.Char      (alphaNumChar, char, digitChar,
                                             newline, printChar, punctuationChar,
                                             space1, string, symbolChar)
 
--- | webdav.yandex.ru == Domain [ "ru", "yandex", "webdav" ]
-newtype Domain = Domain [Text]
-  deriving (Show, Ord, Eq)
+newtype Domain = Domain { unDomain :: Text }
+  deriving newtype (Show, Ord, Eq, Read)
 
 -- | Only IPv4 for now. We does drop AAAA dns responces.
-data IP = IP Text
-  deriving (Show, Ord, Eq)
-
-data HostMap = HostMap
-  Domain
-  IP
-  deriving (Show, Ord, Eq)
+newtype IP = IP Text
+  deriving newtype (Show, Ord, Eq)
 
 type Parser = Parsec Void Text
 
-hostMapsParser :: Parser [HostMap]
-hostMapsParser = concat <$> many (try rowParser <|> dumbRowParser)
+domainsParser :: Parser [Domain]
+domainsParser = catMaybes <$> many (try rowParser <|> dumbRowParser)
 
 -- Row contains zero usable info for us but match any string
-dumbRowParser :: Parser [HostMap]
+dumbRowParser :: Parser (Maybe Domain)
 dumbRowParser = do
   _ <- many printChar
   _ <- newline
-  return []
+  pure Nothing
 
 -- Desired strings look like
--- Jan  7 21:10:35 nixorn-Lenovo-G50-70 tshark[7873]:  1200 76.489068007  192.168.1.1 → 192.168.1.40 DNS 336 Standard query response 0xfce2 A webdav.yandex.ru A 213.180.204.148 NS ns9.z5h64q92x9.net NS ns2.yandex.ru NS ns1.yandex.ru A 154.47.36.189 A 80.239.201.9 AAAA 2001:978:7401::78 AAAA 2001:2030:0:40:8000::78 A 213.180.193.1 AAAA 2a02:6b8::1 A 93.158.134.1 AAAA 2a02:6b8:0:1::1
-rowParser :: Parser [HostMap]
+-- Jan 12 15:20:11 nixorn-Lenovo-G50-70 tshark[6323]: 22272 602.102713466 192.168.1.40 → 192.168.1.1  DNS 85 Standard query 0x5be1 A www.google.com OPT
+-- Jan 12 15:20:11 nixorn-Lenovo-G50-70 tshark[6323]: 22273 602.102872063 192.168.1.40 → 192.168.1.1  DNS 85 Standard query 0xaa45 AAAA www.google.com OPT
+rowParser :: Parser (Maybe Domain)
 rowParser = do
   _ <- textParser -- Jan
   _ <- space1
@@ -67,38 +66,23 @@ rowParser = do
   _ <- space1
   _ <- intParser  -- 336
   _ <- space1
-  _ <- string "Standard query response"
+  _ <- string "Standard query"
   _ <- space1
   _ <- textParser -- 0xfce2
   _ <- space1
-  results <- dnsResponseParser
-  return results
-
-dnsResponseParser :: Parser [HostMap]
-dnsResponseParser = do
-  _ <- string "A"
+  _ <- string "AAAA" <|> string "A"
   _ <- space1
   domain <- domainParser
-  _ <- space1
-  ips <- many aRecordParser
-  _ <- many printChar -- skip rest of line
   _ <- newline
-  return $ map (HostMap domain) ips
+  return $ Just domain
 
 domainParser :: Parser Domain
 domainParser = do
-  topLevelName <- pack <$> many alphaNumChar
+  topLevelName <- T.pack <$> many alphaNumChar
   restNames <- many $ do
     _ <- char '.'
-    pack <$> many alphaNumChar
-  return . Domain . reverse $ topLevelName : restNames
-
-aRecordParser :: Parser IP
-aRecordParser = do
-  _ <- char 'A'
-  _ <- space1
-  ip <- ipParser
-  return ip
+    T.pack <$> many alphaNumChar
+  return . Domain $ T.intercalate "." (topLevelName : restNames)
 
 ipParser :: Parser IP
 ipParser = do
@@ -112,7 +96,7 @@ ipParser = do
   return (IP $ one <> "." <> two <> "." <> three <> "." <> four)
 
 intParser :: Parser Text
-intParser = pack <$> many digitChar
+intParser = T.pack <$> many digitChar
 
 textParser :: Parser Text
-textParser = pack <$> many (alphaNumChar <|> punctuationChar)
+textParser = T.pack <$> many (alphaNumChar <|> punctuationChar)
